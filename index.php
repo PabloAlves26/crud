f1d1535c3d23d424f271c2d04cbe96fb761a24fd
PabloAlves26 <pablo26alves@gmail.com>
2021-12-10 18:29:27 -0200

    Arquivo index comentado e doc de conexão remota criado.

--- index.php
+++ index.php
@@ -1,80 +1,89 @@
 <?php 
 
-//Conexão
-include_once 'php_action/db_connect.php';
-//Header
-include_once 'includes/header.php';
-//Mensagem
-include_once 'includes/mensagem.php';
-//Delete
+    //Conexão - Inclui o arquivo db_connect.php
+    include_once 'php_action/db_connect.php';
+    //Menasagem - Inclui o arquivo mensagem.php
+    include_once 'includes/mensagem.php';
+    //Header - Inclui o header ( cabeçalho no documento atual)
+    include_once 'includes/header.php';
 
 ?>
-<div class="row">
-    <div class="col s12 m6 push-m3">
-        <h3 class="light">Clientes</h3>
-        <table class="striped">
-            <thead>
-                <tr>
-                    <th>Nome:</th>
-                    <th>Sobrenome:</th>
-                    <th>E-mail:</th>
-                    <th>Idade:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $sql = "SELECT*FROM clientes";
-                    $resultado = mysqli_query($connect, $sql);
 
-                    if (mysqli_num_rows($resultado) > 0):
+<!--Inicio do body.-->
+    <div class="row"> <!--Div Classe Linhas-->
+    <div class="col s12 m6 push-m3"><!--Especifica o numero de colunas de acorddo com o tamanho da tela-->
+        <h3 class="light">Clientes</h3><!--Texto Titulo H3-->
+        <table class="hoghtlight striped"><!--Classe do FW que adiciona efeito zebrado na lista-->
+            <thead><!--Inicio cabeçalho tabela-->
+                <tr><!--Define uma linha com conteudos th-->
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>E-mail</th>
+                    <th>Idade</th>
+                </tr>
+            </thead><!--Fim cabeçalho da tabela-->
+
+            <tbody><!--Inicio corpo tabela-->
+
+                <?php /*Inicio codigo php que trata o caso de não haver elementos noBD*/
+                    $sql = "SELECT*FROM clientes";/*Seleciona a tabela no BD  e salva na var $sql*/
+                    $resultado = mysqli_query($connect, $sql);/*Adiciona a conexão a var $resultado*/
+
+                    if (mysqli_num_rows($resultado) > 0):/*Se o resultado for > que 0 o wile é exec*/
 
                     while ($dados = mysqli_fetch_array($resultado)):
-
                 ?>
-                <tr>
-                    <td><?php echo $dados['nome']; ?></td>
-                    <td><?php echo $dados['sobrenome']; ?></td>
-                    <td><?php echo $dados['email']; ?></td>
-                    <td><?php echo $dados['idade']; ?></td>
-                    <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
 
+                <tr><!--Linha com conteudo adicionado dinamicamente vidas do BD-->
+                    <td><?php echo $dados['nome']; ?></td><!--Imprime nome salvo na var $dado-->
+                    <td><?php echo $dados['sobrenome']; ?></td><!--Imprime sobrenome salvo na var $dado-->
+                    <td><?php echo $dados['email']; ?></td><!--Imprime email salvo na var $dado-->
+                    <td><?php echo $dados['idade']; ?></td><!--Imprime idade salvo na var $dado-->
+
+                    <!--Botão Editar com classe do FW e comando para selecionar o  id salvo da var $dados-->
+                    <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                    <!--Botão Deletar com a classe do FW e o comando para selecionar o id salvo na var $dados-->
                     <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
                 </tr>
                 
-                  <!-- Modal Structure -->
-  <div id="modal<?php echo $dados['id']; ?>" class="modal">
-    <div class="modal-content">
-      <h4>Opa!</h4>
-      <p>Têm certeza que deseja excluir esse cliente?</p>
-    </div>
-    <div class="modal-footer">
+                <!-- Janela Modal do FW -->
+                <div id="modal<?php echo $dados['id']; ?>" class="modal">
+                    <div class="modal-content">
+                        <h4>Opa!</h4>
+                        <p>Têm certeza que deseja excluir esse cliente?</p>
+                </div>
+                <div class="modal-footer">
+                <!--Fim Janela Model-->
 
-    <form action="php_action/delete.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
-        <button type="dubmit" name="btn-deletar" class="btn red">Sim, quero deletar.</button>
-
-        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
-    </form>
-    </div>
-  </div>
-                <?php endwhile;
-                    else: ?>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <?php 
-                    endif;
-                 ?>
+                <!--form com input do tipo hidden com o id do dado a ser excluido-->
+                <form action="php_action/delete.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                    <button type="dubmit" name="btn-deletar" class="btn red">Sim, quero deletar.</button>
+            
+                    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+                </form>
+                <!--Fim form com inpult oculto-->
+            </div><!--Fim div que especifica o numero de colunas de acordo com o tam da tela-->
+        </div><!--Fim div class linhas-->
+            <!--Código que trata o caso de naõ haver dados no BD-->
+            <?php endwhile;
+            else: ?>
+                <tr>
+                    <td>Não ha dados</td>
+                    <td>Não ha dados</td>
+                    <td>Bão ha dados</td>
+                    <td>Não ha dados</td>
+                </tr>
+            <?php endif; ?>
+             <!--Fim Código que trata o caso de naõ haver dados no BD-->
             </tbody>
         </table>
         <br>
         <a href="adicionar.php" class="btn">Adicionar Cliente</a>
-    </div>
-</div>
+        </div><!--Fim div tabela-->
+    </div><!--Fim div classe linhas-->
 
-<?php
+<?php /*Adiciona o footer ao documento*/
     include_once 'includes/footer.php';
 ?>
+<!-- Fim body  -->
